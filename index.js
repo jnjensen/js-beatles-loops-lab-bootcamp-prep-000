a67044dc0d24509ced499f3633b7363abb5d4f3f
James Jensen <jnjensen@live.com>
2020-02-05 03:24:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var play = []
-  for (i = 0; i <= musicians.length; i++){
+  for (var i = 0; i <= musicians.length; i++){
     play.push(`${musicians[i]} plays ${instruments[i]}.`)
   }
   return play
